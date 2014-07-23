92d1708ad4ec8e8b095b4b1e6856b7ff81d79b15
wpdemobuilder <wpdemobuilder@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-23 03:16:39 +0000

    Tag 1.0.0 release
    git-svn-id: https://plugins.svn.wordpress.org/wp-demo-builder@953202 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/1.0.0/main.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Plugin Name: WP Demo Builder
+ * Plugin URI:  http://www.wpdemobuilder.com/
+ * Description: Create base site package, push it to WP Demo Builder system and display demo builder panel with ease.
+ * Version:     1.0.0
+ * Author:      WPDemoBuilder Team <admin@wpdemobuilder.com>
+ * Author URI:  http://www.wpdemobuilder.com/
+ * License:     GNU/GPL v2 or later http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+// Define path to this plugin's main file
+define( 'WPDB_PLUGIN', __FILE__ );
+
+// Define text domain
+define( 'WPDB_TEXT', 'wp-demo-builder' );
+
+// Load core class file
+include_once dirname( WPDB_PLUGIN ) . '/includes/core.php';
+
+// Hook into WordPress
+WPDB_Demo_Builder::hook();
